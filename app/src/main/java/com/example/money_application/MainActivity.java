d059bbf084f43bcb44800f059d968ad39d137a6e
esezdi <ercansezdizero@gmail.com>
2019-12-10 14:41:06 +0300

    last changes

--- app/src/main/java/com/example/money_application/MainActivity.java
+++ app/src/main/java/com/example/money_application/MainActivity.java
@@ -3,15 +3,14 @@ package com.example.money_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     float money = 0;
@@ -24,32 +23,84 @@ public class MainActivity extends AppCompatActivity {
 
         Button gelir = (Button) findViewById(R.id.gelir);
         Button gider = (Button) findViewById(R.id.gider);
-        TextView show_money = (TextView) findViewById(R.id.show);
+        TextView show_money = (TextView) findViewById(R.id.delete_show);
         show_money.setText("Balance : " + String.valueOf(money));
-    }
 
+        SQLiteDatabase mydatabase = openOrCreateDatabase("test_x",MODE_PRIVATE,null);
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS TutorialsPoint(islem VARCHAR,para VARCHAR,tur VARCHAR);");
+
+    }
 
     public void on_click_income(View view)
     {
-        TextView show_money = (TextView) findViewById(R.id.show);
+        String islemler,paralar,turu;
+        TextView show_money = (TextView) findViewById(R.id.delete_show);
         EditText money_amount= (EditText) findViewById(R.id.input);
-        money = money + Integer.parseInt(money_amount.getText().toString());
-        show_money.setText("Balance : " + String.valueOf(money));
+        EditText money_operation = (EditText) findViewById(R.id.islem);
+        if(money_operation.getText().toString().equals(""))
+        {
+            Toast toast_2 = Toast.makeText(getApplicationContext(),"Please input operation" ,Toast.LENGTH_SHORT);
+            toast_2.show();
+        }
+        else {
+            if (money_amount.getText().toString().equals("")) {
+                Toast toast_2 = Toast.makeText(getApplicationContext(), "Please input money", Toast.LENGTH_SHORT);
+                toast_2.show();
+            }
+            else {
+                money = money + Integer.parseInt(money_amount.getText().toString());
+                show_money.setText("Balance : " + String.valueOf(money));
+                islemler = money_operation.getText().toString();
+                paralar = money_amount.getText().toString();
+                turu = "income";
+                SQLiteDatabase mydatabase = openOrCreateDatabase("test_x",MODE_PRIVATE,null);
+                String code = "INSERT INTO TutorialsPoint (islem,para,tur) VALUES('"+islemler+"', '"+paralar+"','"+turu+"');";
+                mydatabase.execSQL(code);
+                mydatabase.close();
+
+            }
+        }
     }
-    public void on_click_expense(View view)
-    {
-        TextView show_money = (TextView) findViewById(R.id.show);
-        EditText money_amount= (EditText) findViewById(R.id.input);
-        money = money - Integer.parseInt(money_amount.getText().toString());
-        if(money > 0)
-            show_money.setText("Balance : " + String.valueOf(money));
-        else
-            show_money.setText("Balance : 0");
+    public void on_click_expense(View view) {
+        TextView show_money = (TextView) findViewById(R.id.delete_show);
+        EditText money_amount = (EditText) findViewById(R.id.input);
+        EditText money_operation = (EditText) findViewById(R.id.islem);
+        String paralar,turu,islemler;
+        if(money_operation.getText().toString().equals(""))
+        {
+            Toast toast_2 = Toast.makeText(getApplicationContext(),"Please input operation" ,Toast.LENGTH_SHORT);
+            toast_2.show();
+        }
+        else {
+            if (money_amount.getText().toString().equals("")) {
+                Toast toast_2 = Toast.makeText(getApplicationContext(), "Please input money", Toast.LENGTH_SHORT);
+                toast_2.show();
+            }
+            else {
+                money = money - Integer.parseInt(money_amount.getText().toString());
+                if (money > 0) {
+                    show_money.setText("Balance : " + String.valueOf(money));
+                    islemler = money_operation.getText().toString();
+                    paralar = money_amount.getText().toString();
+                    turu = "expense";
+                    SQLiteDatabase mydatabase = openOrCreateDatabase("test_x", MODE_PRIVATE, null);
+                    String code = "INSERT INTO TutorialsPoint (islem,para,tur) VALUES('" + islemler + "', '" + paralar + "','" + turu + "');";
+                    mydatabase.execSQL(code);
+                    mydatabase.close();
+                }
+                else {
+                    show_money.setText("Balance : 0.0");
+                    money = 0;
+                }
+
+            }
+        }
     }
     public void on_click_transactions(View view)
     {
         Toast toast = Toast.makeText(getApplicationContext(),"transactions",Toast.LENGTH_SHORT);
         toast.show();
+
         startActivity(new Intent(MainActivity.this, islemler.class));
     }
 
--- app/src/main/java/com/example/money_application/islemler.java
+++ app/src/main/java/com/example/money_application/islemler.java
@@ -3,22 +3,78 @@ package com.example.money_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class islemler extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_islemler);
 
+        List<String> islem = new ArrayList<String>();
+        List<String> money = new ArrayList<String>();
+        List<String> tur   = new ArrayList<String>();
 
+        SQLiteDatabase mydatabase = openOrCreateDatabase("test_x",MODE_PRIVATE,null);
+        Cursor cursor = mydatabase.rawQuery("Select * from TutorialsPoint",null);
+        if (cursor.moveToFirst()) {
+            do {
+                islem.add(cursor.getString(0));
+                money.add(cursor.getString(1) + "TL");
+                tur.add(cursor.getString(2));
+            } while (cursor.moveToNext());
+        }
+        ListView listemiz = (ListView) findViewById(R.id.listview_1);
+
+        final ArrayAdapter<String> veriAdaptoru = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, tur){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                View view = super.getView(position,convertView,parent);
+                if(position % 2 == 1)
+                    view.setBackgroundColor(Color.parseColor("#F80000"));
+                else
+                    view.setBackgroundColor(Color.parseColor("#00BCD4"));
+
+                return view;
+            }
+        };
+        listemiz.setAdapter(veriAdaptoru);
+
+        ListView liste = (ListView) findViewById(R.id.listview_2);
+
+        final ArrayAdapter<String> veriAdaptor = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, money){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent3){
+                View view = super.getView(position,convertView,parent);
+                if(position % 2 == 1)
+                    view.setBackgroundColor(Color.parseColor("#F80000"));
+                else
+                    view.setBackgroundColor(Color.parseColor("#00BCD4"));
+                return view;
+            }
+        };
+        liste.setAdapter(veriAdaptor);
 
     }
+
 
     public void come_back(View view)
     {
         startActivity(new Intent(islemler.this, MainActivity.class));
     }
+
 }
